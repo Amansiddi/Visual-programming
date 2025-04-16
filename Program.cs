c6d292777596b0b8abc5f736fbef15fe14bd4ad1
Amansiddi <amansiddique302@gmail.com>
2025-04-16 14:23:43 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string x,y;
-			x=Console.ReadLine();
-			y=Console.ReadLine();
-			int a=int.Parse(x);
-			int b=int.Parse(y);
-			int SUM=a+b;
-			Console.WriteLine("Sum = "+SUM);
+            string H = "Aman";
+            Console.WriteLine(H.ToUpper());
+            Console.WriteLine(H.ToLower());
+            Console.WriteLine(H.IndexOf("a"));
+            Console.WriteLine(H.Length);
         }
     }
 }
