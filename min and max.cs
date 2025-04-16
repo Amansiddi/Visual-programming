d49a8559ba97248c15df5bbde121f3b4f6d833fd
Amansiddi <amansiddique302@gmail.com>
2025-04-16 14:28:29 +0500

    Add files via upload

---
+++ min and max.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int min = -2");
+            Console.WriteLine("int max = 2");
+
+            Console.WriteLine("long min = -9");
+            Console.WriteLine("long max = 9");
+
+            Console.WriteLine("double min = -1.738");
+            Console.WriteLine("double max = 1.7308");
+
+            string a = "Sadiq";
+            string b = "Aman";
+
+            Console.WriteLine("string min = " + (a.CompareTo(b) < 0 ? a : b));
+            Console.WriteLine("string max = " + (a.CompareTo(b) > 0 ? a : b));
+        }
+    }
+}
