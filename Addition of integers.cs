5ece27e6d3dd80e4bd9900d19a3b310a16cd5a71
Amansiddi <amansiddique302@gmail.com>
2025-04-16 14:13:22 +0500

    Add files via upload

---
+++ Addition of integers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x, y;
+            x = Console.ReadLine();
+            y = Console.ReadLine();
+            int a = int.Parse(x);
+            int b = int.Parse(y);
+            int SUM = a + b;
+            Console.WriteLine("Sum = " + SUM);
+        }
+    }
+}
