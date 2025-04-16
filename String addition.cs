e3788fbb18e656f9bfe7cb637dcd691002c761f8
Amansiddi <amansiddique302@gmail.com>
2025-04-16 14:16:21 +0500

    Add files via upload

---
+++ String addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x, y, SUM;
+            x = Console.ReadLine();
+            y = Console.ReadLine();
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            SUM = x + y;
+            Console.WriteLine("Sum = " + SUM);
+        }
+    }
+}
