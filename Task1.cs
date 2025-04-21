7df3239613f951d69e72653ce834af85ed5506bb
Amansiddi <amansiddique302@gmail.com>
2025-04-21 12:41:58 +0500

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string personName;
+            personName = "AMAN";
+            int personAge = 25;
+            Console.WriteLine(personName);
+            Console.WriteLine(personAge);
+        }
+    }
+}
---
+++ task 3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            void age()
+            {
+                int personAge;//Local Variable
+                personAge = 25;
+                Console.WriteLine(personAge);
+            }
+
+            string personName; //Global Variable
+            personName = "Aman siddique";
+            Console.WriteLine(personName);
+            age();
+        }
+    }
+}
